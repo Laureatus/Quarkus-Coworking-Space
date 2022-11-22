6e957f2c1d79cb7b8765fbde4bd7ad059cf1d539
Laureatus <just4like04@gmail.com>
2022-11-22 10:37:45 +0100

    Add getters,setters and variables to Booking.java

--- src/main/java/ch/zli/m223/model/Booking.java
+++ src/main/java/ch/zli/m223/model/Booking.java
@@ -1,5 +1,45 @@
 package ch.zli.m223.model;
 
-public class Booking {
+import java.time.LocalDateTime;
 
+import javax.persistence.Entity;
+
+@Entity
+public class Booking {
+    private Long id;
+    private LocalDateTime bookedDate;
+    private ApplicationUser user;
+    private boolean wholeDay;
+    private boolean halfDay;
+
+    public Long getId() {
+        return id;
+    }
+    public void setId(Long id) {
+        this.id = id;
+    }
+    public LocalDateTime getBookedDate() {
+        return bookedDate;
+    }
+    public void setBookedDate(LocalDateTime bookedDate) {
+        this.bookedDate = bookedDate;
+    }
+    public ApplicationUser getUser() {
+        return user;
+    }
+    public void setUser(ApplicationUser user) {
+        this.user = user;
+    }
+    public boolean isWholeDay() {
+        return wholeDay;
+    }
+    public void setWholeDay(boolean wholeDay) {
+        this.wholeDay = wholeDay;
+    }
+    public boolean isHalfDay() {
+        return halfDay;
+    }
+    public void setHalfDay(boolean halfDay) {
+        this.halfDay = halfDay;
+    }
 }
