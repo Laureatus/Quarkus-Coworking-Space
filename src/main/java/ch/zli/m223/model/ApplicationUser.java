46b1c14fd6210656ff77d2e36f2e5776d32c76d9
Lorin Fankhauser <lorin.fankhauser@unic.com>
2022-11-22 09:51:29 +0100

    Create project relevant entities

---
+++ src/main/java/ch/zli/m223/model/ApplicationUser.java
@@ -0,0 +1,27 @@
+package ch.zli.m223.model;
+
+import java.util.Set;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+
+@Entity
+public class ApplicationUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+    
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private boolean isAdmin;
+    private Set<Booking> bookings;
+    private Set<Material> materials;
+    private Set<Coffee> coffee;
+}
