6802a17f679e1c56738ca4261a688cb348cc7d3b
Laureatus <just4like04@gmail.com>
2022-11-22 11:31:29 +0100

    Add getters and setters to coffee.java

--- src/main/java/ch/zli/m223/model/Coffee.java
+++ src/main/java/ch/zli/m223/model/Coffee.java
@@ -1,5 +1,55 @@
 package ch.zli.m223.model;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
+@Entity
 public class Coffee {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Schema(readOnly = true)
+    private Long id;
+
+    @Column
+    private LocalDateTime time;
+
+    @ManyToOne(optional = false)
+    @Fetch(FetchMode.JOIN)
+    private ApplicationUser user;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getTime() {
+        return time;
+    }
+
+    public void setTime(LocalDateTime time) {
+        this.time = time;
+    }
+
+    public ApplicationUser getUser() {
+        return user;
+    }
+
+    public void setUser(ApplicationUser user) {
+        this.user = user;
+    }
+
 
 }
