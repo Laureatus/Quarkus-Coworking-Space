ff6d1809f5c0bd030b00d01f9ca0b859633e38c6
Laureatus <just4like04@gmail.com>
2022-11-22 13:12:06 +0100

    Add import.sql

---
+++ src/main/resources/import.sql
@@ -0,0 +1,23 @@
+INSERT INTO public.applicationuser(
+	id, email, firstname, isadmin, lastname, password)
+	VALUES (1, 'test@gmail.com', 'lorin', true, 'fankhauser', 'password123');
+
+INSERT INTO public.applicationuser(
+	id, email, firstname, isadmin, lastname, password)
+	VALUES (2, 'test2@gmail.com', 'Max', false, 'Mustermann', 'max123');
+
+INSERT INTO public.booking(
+	id, bookeddate, halfday, wholeday, user_id)
+	VALUES (1, '2019-01-21T05:47:08.644', true, false, 1);
+
+INSERT INTO public.coffee(
+	id, "time", user_id)
+	VALUES (1, '2019-01-21T05:47:08.644', 1);
+
+INSERT INTO public.lunch(
+	id, name, supplier, user_id)
+	VALUES (1, 'pizza', 'PizzaHut', 1);
+
+INSERT INTO public.material(
+	id, name, user_id)
+	VALUES (1, 'usb type C charger_1', 1);
