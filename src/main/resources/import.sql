79f901692e30a38dcd0636189eb00df4864a0cef
Laureatus <just4like04@gmail.com>
2022-11-23 09:36:36 +0100

    Remove id's from insert statements

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,35 +1,35 @@
 INSERT INTO public.applicationuser(
-	id, email, firstname, isadmin, lastname, password)
-	VALUES (1, 'test@gmail.com', 'lorin', true, 'fankhauser', 'password123');
+	email, firstname, isadmin, lastname, password)
+	VALUES ('test@gmail.com', 'lorin', true, 'fankhauser', 'password123');
 
 INSERT INTO public.applicationuser(
-	id, email, firstname, isadmin, lastname, password)
-	VALUES (2, 'test2@gmail.com', 'Max', false, 'Mustermann', 'max123');
+	email, firstname, isadmin, lastname, password)
+	VALUES ('test2@gmail.com', 'Max', false, 'Mustermann', 'max123');
 
 INSERT INTO public.booking(
-	id, bookeddate, halfday, wholeday, user_id)
-	VALUES (1, '2019-01-21T05:47:08.644', true, false, 1);
+	bookeddate, halfday, wholeday, user_id)
+	VALUES ('2019-01-21T05:47:08.644', true, false, 1);
 
 INSERT INTO public.booking(
-	id, bookeddate, halfday, wholeday, user_id)
-	VALUES (2, '2019-01-21T05:47:08.644', true, false, 2);
+	bookeddate, halfday, wholeday, user_id)
+	VALUES ('2019-01-21T05:47:08.644', true, false, 2);
 
 INSERT INTO public.booking(
-	id, bookeddate, halfday, wholeday, user_id)
-	VALUES (3, '2019-01-21T05:47:08.644', true, false, 2);
+	bookeddate, halfday, wholeday, user_id)
+	VALUES ('2019-01-21T05:47:08.644', true, false, 2);
 
 INSERT INTO public.booking(
-	id, bookeddate, halfday, wholeday, user_id)
-	VALUES (4, '2019-01-21T05:47:08.644', false, true, 2);
+	bookeddate, halfday, wholeday, user_id)
+	VALUES ('2019-01-21T05:47:08.644', false, true, 2);
 
 INSERT INTO public.coffee(
-	id, "time", user_id)
-	VALUES (1, '2019-01-21T05:47:08.644', 1);
+	"time", user_id)
+	VALUES ('2019-01-21T05:47:08.644', 1);
 
 INSERT INTO public.lunch(
-	id, name, supplier, user_id)
-	VALUES (1, 'pizza', 'PizzaHut', 1);
+	name, supplier, user_id)
+	VALUES ('pizza', 'PizzaHut', 1);
 
 INSERT INTO public.material(
-	id, name, user_id)
-	VALUES (1, 'usb type C charger_1', 1);
+	name, user_id)
+	VALUES ('usb type C charger_1', 1);
