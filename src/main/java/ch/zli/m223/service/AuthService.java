23f332bbe4c7d6eff675c660aecd60d5795a9b83
Laureatus <just4like04@gmail.com>
2022-11-23 15:19:01 +0100

    Improve AuthService

--- src/main/java/ch/zli/m223/service/AuthService.java
+++ src/main/java/ch/zli/m223/service/AuthService.java
@@ -1,6 +1,9 @@
 package ch.zli.m223.service;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+import javax.transaction.Transactional;
 
 import org.eclipse.microprofile.jwt.Claims;
 
@@ -9,29 +12,39 @@ import io.smallrye.jwt.build.Jwt;
 
 @ApplicationScoped
 public class AuthService {
+
+  @Inject
+  EntityManager entityManager;
     /**
      * Generate JWT token
      */
     public String generateToken(ApplicationUser applicationUser) {
+      var email = applicationUser.getEmail();
       if (applicationUser.isAdmin()) {
         String token =
            Jwt.issuer("https://example.com/issuer") 
-             .upn("jdoe@quarkus.io") 
+             .upn(email) 
              .groups("Admin") 
-             .claim(Claims.birthdate.name(), "2001-07-13") 
-             .expiresIn(86400)
+             .claim(Claims.given_name.name(), applicationUser.getId())
+             .expiresIn(Integer.MAX_VALUE)
            .sign();
            return token;
       }
       else {
         String token =
            Jwt.issuer("https://example.com/issuer") 
-             .upn("jdoe@quarkus.io") 
+             .upn(email) 
              .groups("User") 
-             .claim(Claims.birthdate.name(), "2001-07-13") 
-             .expiresIn(86400)
+             .claim(Claims.given_name.name(), applicationUser.getId())
+             .expiresIn(Integer.MAX_VALUE)
            .sign();
            return token;
       }
+    }
+
+    @Transactional
+    public String register(ApplicationUser applicationUser) {
+      entityManager.persist(applicationUser);
+      return generateToken(applicationUser);
     }
 }
