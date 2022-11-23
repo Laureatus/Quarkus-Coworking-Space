1e9db2482ff236c10dd51d53a42e24cf7011084f
Laureatus <just4like04@gmail.com>
2022-11-23 15:22:08 +0100

    Add register method

--- src/main/java/ch/zli/m223/controller/ApplicationUserController.java
+++ src/main/java/ch/zli/m223/controller/ApplicationUserController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.annotation.security.RolesAllowed;
 import javax.inject.Inject;
+
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
@@ -12,7 +13,9 @@ import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
 import org.eclipse.microprofile.openapi.annotations.Operation;
 import org.eclipse.microprofile.openapi.annotations.tags.Tag;
@@ -61,6 +64,14 @@ public class ApplicationUserController {
         return authService.generateToken(foundUser);
        }
         return "";
+    }
+
+    @POST
+    @Path("/register")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public String register(ApplicationUser user) {
+       return authService.register(user);
     }
 
     @Path("/{id}")
